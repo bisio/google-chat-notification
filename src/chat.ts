29d06115a545f2a46cbd2de7e546d34aeb6e602c
Andrea Bisognin <andrea.bisognin@gmail.com>
2022-04-17 17:28:19 +0200

    Update chat.ts

--- src/chat.ts
+++ src/chat.ts
@@ -21,7 +21,7 @@ const textButton = (text: string, url: string) => ({
   }
 });
 
-export async function notify(name: string, url: string, status: Status) {
+export async function notify(name: string, url: string, status: Status, extra: string) {
   const { owner, repo } = github.context.repo;
   const { eventName, sha, ref } = github.context;
   const { number } = github.context.issue;
@@ -59,7 +59,10 @@ export async function notify(name: string, url: string, status: Status) {
             },
             {
               keyValue: { topLabel: "ref", content: ref }
-            }
+            },
+            {
+              keyValue: { topLabel: "extra", content: extra }
+            }            
           ]
         },
         {
