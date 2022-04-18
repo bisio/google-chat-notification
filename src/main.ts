330cca8eacc5527a253349b0edd97136b33102ce
Andrea Bisognin <andrea.bisognin@scaiitec.it>
2022-04-18 16:51:47 +0200

    fixed vulnerabilities

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import * as core from '@actions/core';
 import * as JobStatus from './status';
 import * as GoogleChat from './chat';
 
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) {
+    return error.message;
+  }
+  return String(error);
+}
+
 async function run() {
   try {
     const name = core.getInput('name', { required: true });
@@ -14,7 +21,7 @@ async function run() {
     await GoogleChat.notify(name, url, status, extra);
     console.info('Sent message.')
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(getErrorMessage(error));
   }
 }
 
